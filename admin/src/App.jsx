1164dbddeda73bff4fbeb8b6f8bf9082c695051c
raaminkhalili2001@gmail.com <ramin.khalili@studenti.unipd.it>
2024-08-26 14:20:48 +0200

    add page completed

---
+++ admin/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar.jsx";
+import SideBar from "./components/SideBar/SideBar.jsx";
+import {Route, Routes} from "react-router-dom";
+import Add from "./pages/Add/Add.jsx";
+import List from "./pages/List/List.jsx";
+import Orders from "./pages/Orders/Orders.jsx";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App() {
+    return (
+        <div>
+            <ToastContainer />
+            <Navbar/>
+            <hr/>
+            <div className="app-content">
+                <SideBar/>
+
+                <Routes>
+                    <Route path="/add" element={<Add/>}/>
+                    <Route path="/list" element={<List/>}/>
+                    <Route path="/orders" element={<Orders/>}/>
+                </Routes>
+
+            </div>
+        </div>
+    )
+}
+
+export default App;
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,45 +1,73 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import "./Add.css"
 import upload from "../../assets/upload_area.png"
+import axios from "axios";
+import { toast } from 'react-toastify';
 
 function Add() {
     const [image, setImage] = useState(false)
     const [data, setData] = useState({
-        name:"",
-        description:"",
-        category:"salad",
-        price:""
+        name: "",
+        description: "",
+        category: "salad",
+        price: ""
     })
+    const url = "http://localhost:4000"
 
     const onClickHandler = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        setData(data=>({...data, [name]: value}))
+        setData(data => ({...data, [name]: value}))
     }
 
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("name", data.name);
+        formData.append("description", data.description)
+        formData.append("category", data.category)
+        formData.append("price", Number(data.price))
+        formData.append("image", image);
+        const response = await axios.post(`${url}/api/food/add`, formData)
+        if (response.data.success) {
+            setData({
+                name: "",
+                description: "",
+                category: "salad",
+                price: ""
+            })
+            toast.success("Added successfully.");
+            setImage(false)
+        }
+        else {
+            toast.error(response.data.message)
+        }
+    }
 
     return (
         <div className="Add">
-            <form className="flex-col">
+            <form className="flex-col" onSubmit={onSubmitHandler}>
                 <div className="add-image-upload flex-col">
                     <p>Upload Image</p>
                     <label htmlFor="image">
-                        <img src={image?URL.createObjectURL(image):upload} alt=""/>
+                        <img src={image ? URL.createObjectURL(image) : upload} alt=""/>
                     </label>
-                    <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden={true} required/>
+                    <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden={true} required/>
                 </div>
                 <div className="add-product-name flex-col">
                     <p>Product name</p>
-                    <input onChange={onClickHandler} type="text" name="name" placeholder="Type here" required />
+                    <input onChange={onClickHandler} value={data.name} type="text" name="name" placeholder="Type here"
+                           required/>
                 </div>
                 <div className="add-product-description flex-col">
                     <p>Product Description</p>
-                    <textarea onChange={onClickHandler} name="description" placeholder="Description" rows="6" />
+                    <textarea onChange={onClickHandler} value={data.description} name="description"
+                              placeholder="Description" rows="6"/>
                 </div>
                 <div className="add-category-price">
                     <div className="add-category flex-col">
                         <p>Product category</p>
-                        <select name="category" onChange={onClickHandler}>
+                        <select name="category" onChange={onClickHandler} value={data.category}>
                             <option value="Salad">Salad</option>
                             <option value="Rolls">Rolls</option>
                             <option value="Deserts">Deserts</option>
@@ -53,10 +81,11 @@ function Add() {
                     </div>
                     <div className="add-price flex-col">
                         <p>Product price</p>
-                        <input onChange={onClickHandler} type="Number" name="price" placeholder="Price" required />
+                        <input onChange={onClickHandler} value={data.price} type="Number" name="price"
+                               placeholder="Price" required/>
                     </div>
                 </div>
-                <button type="submit" className="add-btn">Add</button>
+                <button type="submit" className="add-btn">ADD</button>
             </form>
         </div>
     )
